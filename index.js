640526a1dbc6a0285bfa7300e8ef9cda70004a7f
Isaac Lal <isaaclal124@gmail.com>
2023-08-16 11:19:24 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// init AOS animation
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
+
+// the additional code you saw in the video will not be needed :)
