a84074b64aef3714381895cb2d4fac3f1f779527
Isaac Lal <isaaclal124@gmail.com>
2023-08-18 15:46:58 -0400

    Completed popular foods section

---
+++ index.js
@@ -0,0 +1,22 @@
+import check from './assets/check.svg';
+import star from './assets/star.svg';
+import sushi12 from './assets/sushi-12.png';
+import sushi11 from './assets/sushi-11.png';
+import sushi10 from './assets/sushi-10.png';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  duration: 1000,
+  offset: 100,
+});
+
+const trendingSushis = [
+  'Make Sushi',
+  'Nigiri Sushi',
+  'Oshizushi',
+  'Temaki Sushi',
+  'Uramaki Sushi',
+  'Inari Sushi',
+];
